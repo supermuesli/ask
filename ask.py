e9671e0913a634c2f1491c746e8f360aaa389a33
supermuesli <art.lal@hotmail.com>
2018-06-17 06:57:50 +0200

    fixed bug

--- ask.py
+++ ask.py
@@ -5,16 +5,19 @@ from bs4 import BeautifulSoup
 
 #print("https://stackoverflow.com/search?q=" + "+".join(sys.argv[1:]))
 
-r = requests.get("https://stackoverflow.com/search?q=" + "+".join(sys.argv[1:]))
+r = requests.get("https://stackoverflow.com/search?tab=votes&q=" + "+".join(sys.argv[1:]))
 soup = BeautifulSoup(r.text, "lxml")
 
 s = soup.findAll('a', href=re.compile("^/questions/"))
-r = requests.get("https://stackoverflow.com" + str(s[1].get("href")))
+if len(s) == 1:
+	print("i don't know :(")
+else:
+	r = requests.get("https://stackoverflow.com" + str(s[1].get("href")))
 
-soup = BeautifulSoup(r.text, "lxml")
-s = soup.find_all("div", {"class": "answer accepted-answer"})
+	soup = BeautifulSoup(r.text, "lxml")
+	s = soup.find_all("div", {"class": "answer accepted-answer"})
 
-for tag in s:
-    tdTags = tag.find_all("div", {"class": "post-text"})
-    for tag in tdTags:
-        print(tag.text)
+	for tag in s:
+		tdTags = tag.find_all("div", {"class": "post-text"})
+		for tag in tdTags:
+			print(tag.text)
