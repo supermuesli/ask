839d0c3250f767a92c140c108ceebc831ff71091
supermuesli <art.lal@hotmail.com>
2018-06-17 08:43:01 +0200

    wow

--- ask.py
+++ ask.py
@@ -32,7 +32,31 @@ soup = BeautifulSoup(r.text, "lxml")
 s = soup.findAll("a", href=re.compile("https://stackoverflow"))
 
 if len(s) == 0:
-	print("i don't know :(")
+	r = requests.get("https://www.google.de/search?q=" + "+".join(sys.argv[1:]) + "+stackoverflow")
+	soup = BeautifulSoup(r.text, "lxml")
+
+	s = soup.findAll("a", href=re.compile("https://stackoverflow"))
+	
+	if len(s) == 0:
+		print("i don't know :(")
+		
+	else:
+		r = requests.get(str(s[0].get("href")).split("url?q=")[1].split("&")[0])
+
+		soup = BeautifulSoup(r.text, "lxml")
+		s = soup.find_all("div", {"class": "answer accepted-answer"})
+		
+		if len(s) == 0:
+			s = soup.find_all("div", {"class": "answer"})[0]
+			tdTags = s.find_all("div", {"class": "post-text"})
+			for tag in tdTags:
+				print(tag.text)
+				
+		else:
+			for tag in s:
+				tdTags = tag.find_all("div", {"class": "post-text"})
+				for tag in tdTags:
+					print(tag.text)
 else:
 	r = requests.get(str(s[0].get("href")).split("url?q=")[1].split("&")[0])
 
