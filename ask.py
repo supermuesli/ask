e5dd085a6a84d4a9636d3d7f9647650f2010da0f
juliusmh <juliusmh@web.de>
2018-06-20 14:44:23 +0200

    major fixes and syntax highlighting

--- ask.py
+++ ask.py
@@ -1,79 +1,37 @@
+#!/usr/bin/env python3
+
 import sys
 import requests
 import re
 from bs4 import BeautifulSoup
 
-#print("https://www.google.de/search?q=" + "+".join(sys.argv[1:]))
-
-#old version
-"""
-r = requests.get("https://stackoverflow.com/search?tab=votes&q=" + str(sys.argv[1]).replace("\"", ""))
-soup = BeautifulSoup(r.text, "lxml")
-
-s = soup.findAll("a", href=re.compile("^/questions/"))
-if len(s) == 1:
-	print("i don't know :(")
-else:
-	r = requests.get("https://stackoverflow.com" + str(s[1].get("href")))
-
-	soup = BeautifulSoup(r.text, "lxml")
-	s = soup.find_all("div", {"class": "answer accepted-answer"})
-
-	for tag in s:
-		tdTags = tag.find_all("div", {"class": "post-text"})
-		for tag in tdTags:
-			print(tag.text)
-"""
-
-#new version
 r = requests.get("https://www.google.de/search?q=" + "+".join(sys.argv[1:]))
 soup = BeautifulSoup(r.text, "lxml")
+s = soup.find("a", href=re.compile("https://stackoverflow"))
 
-s = soup.findAll("a", href=re.compile("https://stackoverflow"))
-
-if len(s) == 0:
+# Nothing found, appending "stackoverflow"
+if s is None:
 	r = requests.get("https://www.google.de/search?q=" + "+".join(sys.argv[1:]) + "+stackoverflow")
 	soup = BeautifulSoup(r.text, "lxml")
-
-	s = soup.findAll("a", href=re.compile("https://stackoverflow"))
+	s = soup.find("a", href=re.compile("https://stackoverflow"))
 	
-	if len(s) == 0:
-		print("i don't know :(")
-		
-	else:
-		r = requests.get(str(s[0].get("href")).split("url?q=")[1].split("&")[0])
+# No results found
+if s is None:
+	print("i don't know :(")
+	exit()
 
-		soup = BeautifulSoup(r.text, "lxml")
-		s = soup.find_all("div", {"class": "answer accepted-answer"})
-		
-		if len(s) == 0:
-			try:
-				s = soup.find_all("div", {"class": "answer"})[0]
-				tdTags = s.find_all("div", {"class": "post-text"})
-				for tag in tdTags:
-					print(tag.text)
-			except:
-				print("i dont know :(")
-				
-		else:
-			for tag in s:
-				tdTags = tag.find_all("div", {"class": "post-text"})
-				for tag in tdTags:
-					print(tag.text)
-else:
-	r = requests.get(str(s[0].get("href")).split("url?q=")[1].split("&")[0])
+s = BeautifulSoup(requests.get(s["href"].split("url?q=")[1].split("&")[0]).text, "lxml")
 
-	soup = BeautifulSoup(r.text, "lxml")
-	s = soup.find_all("div", {"class": "answer accepted-answer"})
-	
-	if len(s) == 0:
-		s = soup.find_all("div", {"class": "answer"})[0]
-		tdTags = s.find_all("div", {"class": "post-text"})
-		for tag in tdTags:
-			print(tag.text)
-			
-	else:
-		for tag in s:
-			tdTags = tag.find_all("div", {"class": "post-text"})
-			for tag in tdTags:
-				print(tag.text)
+
+accepted = s.find("div", {"class": "accepted-answer"})
+
+if accepted is not None:
+	t = accepted.find("div", {"class": "post-text"})
+
+if accepted is None:
+	t = s.find("div", {"class": "answer"})
+
+# Apply Color highlighting
+html = str(t).replace("<code>", "{{code}}").replace("</code>", "{{/code}}")
+print (BeautifulSoup(html, "lxml").text.replace("{{code}}", '\n\033[92m').replace("{{/code}}", '\033[0m'))
+
